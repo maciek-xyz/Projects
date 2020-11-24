50c38fa3f1cc1ae67f91cd261931f36619d1a824
maciek-xyz <maciekrmcf@gmail.com>
2020-11-24 10:31:38 +0100

    Lessons5

---
+++ Lessons5/WordSplit.java
@@ -0,0 +1,14 @@
+package Lesson5;
+
+public class WordSplit {
+    public static void main(String[] args) {
+
+        String text = args[0];
+        char[] a = text.toCharArray();
+
+        for(char c: a){
+            System.out.println(c);
+        }
+
+    }
+}
